64b3e9bdb61c8cfdf3953dc40ff7b6b8a6df6bcf
Jordan Sullivan <helloandhi.jp@gmail.com>
2022-04-25 22:44:19 -0600

    refactor hotel tests and remove pseudocode

--- test/Hotel-test.js
+++ test/Hotel-test.js
@@ -1,16 +1,12 @@
 import { expect } from "chai";
 import Hotel from "../src/classes/Hotel";
 import Guest from "../src/classes/Guest";
-//import Room from "../src/classes/Room";
-//import Booking from "../src/classes/Booking";
+import Room from "../src/classes/Room";
+import Booking from "../src/classes/Booking";
 
 describe("Hotel", () => {
   let customersData,
-    bookingsData,
-    roomsData,
-    guest14,
-    guest15,
-    guest16,
+    guests,
     hotel,
     booking1,
     booking2,
@@ -39,94 +35,6 @@ describe("Hotel", () => {
       },
     ];
 
-    bookingsData = [
-      {
-        id: "5fwrgu4i7k55hl6tn",
-        userID: 15,
-        date: "2022/01/17",
-        roomNumber: 5,
-      },
-      {
-        id: "5fwrgu4i7k55hl6u7",
-        userID: 14,
-        date: "2022/01/16",
-        roomNumber: 17,
-      },
-      {
-        id: "5fwrgu4i7k55hl6w1",
-        userID: 15,
-        date: "2022/02/19",
-        roomNumber: 8,
-      },
-      {
-        id: "5fwrgu4i7k55hl6w2",
-        userID: 14,
-        date: "2022/01/26",
-        roomNumber: 15,
-      },
-      {
-        id: "5fwrgu4i7k55hl6wd",
-        userID: 14,
-        date: "2022/01/12",
-        roomNumber: 22,
-      },
-    ];
-
-    roomsData = [
-      {
-        number: 5,
-        roomType: "single room",
-        bidet: true,
-        bedSize: "queen",
-        numBeds: 2,
-        costPerNight: 340.17,
-      },
-      {
-        number: 17,
-        roomType: "junior suite",
-        bidet: false,
-        bedSize: "twin",
-        numBeds: 2,
-        costPerNight: 328.15,
-      },
-      {
-        number: 8,
-        roomType: "junior suite",
-        bidet: false,
-        bedSize: "king",
-        numBeds: 1,
-        costPerNight: 261.26,
-      },
-      {
-        number: 15,
-        roomType: "residential suite",
-        bidet: false,
-        bedSize: "full",
-        numBeds: 1,
-        costPerNight: 294.56,
-      },
-      {
-        number: 22,
-        roomType: "single room",
-        bidet: false,
-        bedSize: "full",
-        numBeds: 2,
-        costPerNight: 350.31,
-      },
-    ];
-
-    guest14 = new Guest(
-      customersData[0],
-      bookingsData.filter((booking) => booking.userID === 14)
-    );
-    guest15 = new Guest(
-      customersData[1],
-      bookingsData.filter((booking) => booking.userID === 15)
-    );
-    guest16 = new Guest(
-      customersData[2],
-      bookingsData.filter((booking) => booking.userID === 16)
-    );
     hotel = new Hotel(guests, rooms);
 
     room1 = new Room({
@@ -196,7 +104,7 @@ describe("Hotel", () => {
     bookingArray.push(booking1, booking2);
     bookingArray2.push(booking3, booking4);
 
-    guests.push(customersData[0], customersData[1]);
+    guests.push(customersData[0], customersData[1], customersData[2]);
     rooms.push(room1, room2, room3);
   });
 
